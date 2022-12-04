eb22ad1b86f6b497a3a72ea6905378bc89e55348
vdsalex <alexandre.vandensteen@gmail.com>
2022-12-04 14:32:08 +0100

    day 3 part 2

--- src/test/kotlin/Day3Test.kt
+++ src/test/kotlin/Day3Test.kt
@@ -18,12 +18,34 @@ class Day3Test {
                 .first
                 .toCharArray()
                 .first { c -> twoCompartment.second.contains(c) }
-        }.sumOf {letter ->
+        }.sumOf { letter ->
             if (letter.isUpperCase())
                 (letter.code - UPPER_VALUE)
             else
                 (letter.code - LOWER_VALUE)
         }.also { println(it) }
+    }
 
+    @Test
+    fun `rucksack reorganisation, update badge`() {
+        val filePath = AoCUtils.getFilePath("/03/input")
+        val lines = AoCUtils.linesFromFileToList(filePath)
+        val groups = mutableListOf<Triple<String, String, String>>()
+
+        for (i in lines.indices step 3) {
+            groups.add(Triple(lines[i], lines[i+1], lines[i+2]))
+        }
+
+        groups.map { group ->
+            group
+                .first
+                .toCharArray()
+                .first { c -> group.second.contains(c) && group.third.contains(c) }
+        }.sumOf { letter ->
+            if (letter.isUpperCase())
+                (letter.code - UPPER_VALUE)
+            else
+                (letter.code - LOWER_VALUE)
+        }.also { println(it) }
     }
 }
