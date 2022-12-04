6f9f3f82c007c93d803fe3aa00f1b0e9210b5792
vdsalex <alexandre.vandensteen@gmail.com>
2022-12-04 21:35:15 +0100

    day 4 part 2

--- src/main/kotlin/model/Range.kt
+++ src/main/kotlin/model/Range.kt
@@ -5,6 +5,8 @@ data class Range(val start: Int, val end: Int) {
     fun fullyContains(otherRange: Range): Boolean {
         if (start <= otherRange.start && end >= otherRange.end)
             return true
+        if (end >= otherRange.start && end <= otherRange.end)
+            return true
         return false
     }
 }
